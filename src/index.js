649cd9959e02f0ba33d247d541ecb8b7f0db2bad
sutter <laurent@sutterlity.fr>
2017-01-16 11:30:46 +0100

    Show first quote

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import quoteData from './data/quote.const';
 import './index.css';
+
+class App extends Component {
+
+  state = {
+    quote: quoteData[0]
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <div className="quote">
+          <div className="quote-text">
+            { this.state.quote.text }
+          </div>
+          <div className="quote-author">
+            { this.state.quote.author }
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
