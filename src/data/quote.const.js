4f67be825966fc6138c01cd95c8de3cdef94483b
sutter <laurent@sutterlity.fr>
2017-01-16 11:07:51 +0100

    Add quote data

---
+++ src/data/quote.const.js
@@ -0,0 +1,54 @@
+const QuoteData = [
+  {
+    id: 1,
+    text: "Exige beaucoup de toi-même et attends peu des autres. Ainsi beaucoup d'ennuis te seront épargnés.",
+    author: "Confucius",
+  },
+  {
+    id: 2,
+    text: "Dans la vie on ne fait pas ce que l'on veut mais on est responsable de ce que l'on est.",
+    author: "Jean-Paul Sartre",
+  },
+  {
+    id: 3,
+    text: "La règle d'or de la conduite est la tolérance mutuelle, car nous ne penserons jamais tous de la même façon, nous ne verrons qu'une partie de la vérité et sous des angles différents.",
+    author: "Gandhi",
+  },
+  {
+    id: 4,
+    text: "La vie est un mystère qu'il faut vivre, et non un problème à résoudre.",
+    author: "Gandhi",
+  },
+  {
+    id: 5,
+    text: "Pour critiquer les gens il faut les connaître, et pour les connaître, il faut les aimer.",
+    author: "Coluche",
+  },
+  {
+    id: 6,
+    text: "On passe une moitié de sa vie à attendre ceux qu'on aimera et l'autre moitié à quitter ceux qu'on aime.",
+    author: "Victor Hugo",
+  },
+  {
+    id: 7,
+    text: "Le monde est dangereux à vivre ! Non pas tant à cause de ceux qui font le mal, mais à cause de ceux qui regardent et laissent faire.",
+    author: "Albert Einstein",
+  },
+  {
+    id: 8,
+    text: "La vie, c'est comme une bicyclette, il faut avancer pour ne pas perdre l'équilibre.",
+    author: "Albert Einstein",
+  },
+  {
+    id: 9,
+    text: "Le bonheur c'est lorsque vos actes sont en accord avec vos paroles.",
+    author: "Indira Gandhi",
+  },
+  {
+    id: 10,
+    text: "On ne souffre jamais que du mal que nous font ceux qu'on aime. Le mal qui vient d'un ennemi ne compte pas.",
+    author: "Victor Hugo",
+  },
+];
+
+export default QuoteData;
